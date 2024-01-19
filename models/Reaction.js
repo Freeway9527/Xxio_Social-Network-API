b45de91eb737bb4cdb0f77f52976a2f7e442354a
xiongxeng <xiongxeng@gmail.com>
2024-01-19 15:25:55 -0600

    add routes and models for thought/reaction

---
+++ models/Reaction.js
@@ -0,0 +1,42 @@
+const {Schema, model} = require('mongoose');
+
+// Define the thought schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        // Embed reactions in the thought model
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Calculate the number of reactions
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// Create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// Export the Thought model
+module.exports = Thought;
+
